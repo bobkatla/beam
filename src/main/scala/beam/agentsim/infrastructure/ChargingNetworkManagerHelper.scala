7643575dd28131b8c7c1c7bc2263ae3cc54db8da
Rutvik Patel <heyrutvik@gmail.com>
2022-05-30 16:05:23 +0530

    fixed ridehail failure

--- src/main/scala/beam/agentsim/infrastructure/ChargingNetworkManagerHelper.scala
+++ src/main/scala/beam/agentsim/infrastructure/ChargingNetworkManagerHelper.scala
@@ -76,7 +76,7 @@ trait ChargingNetworkManagerHelper extends {
   ): Option[ScheduleTrigger] = {
     assume(endTime - startTime >= 0, s"timeInterval should not be negative! startTime $startTime endTime $endTime")
     // Calculate the energy to charge each vehicle connected to the a charging station
-    val endOfMaxSessionTime = chargingEndTimeInSeconds(chargingVehicle.personId)
+    val endOfMaxSessionTime = chargingEndTimeInSeconds.getOrElse(chargingVehicle.personId, endTime)
     val endOfShiftTime = chargingVehicle.chargingShouldEndAt.map(_ - parallelismWindow).getOrElse(endTime)
     val updatedEndTime = Math.min(endOfMaxSessionTime, endOfShiftTime)
     val duration = Math.max(0, updatedEndTime - startTime)
